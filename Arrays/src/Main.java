93facdb7a848ffacbae95665dc34185bcbb651cd
Jbasilio55 <j.l.basilio55@gmail.com>
2023-04-11 22:16:07 -0400

    Arrays description and examples

---
+++ Arrays/src/Main.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        // Why we need arrays? (a collection of a particular data type)
+        //
+        // Arrays are used to store multiple values in a single variable, instead of declaring separate variables
+        // for each value.
+        //
+        //To declare an array, define the variable type with square brackets:
+        //
+        //syntax
+        //datatype[] variableName = new datatype[size]
+
+        //store 5 names:
+        String[] names = new String[5];
+
+        //names are added to names array
+        names[0] = "Jorge";
+        names[1] = "Tom";
+        names[2] = "Jimmy";
+        names[3] = "William";
+
+        System.out.println(Arrays.asList(names));
+        // all names are added and because the size is 5, the left over is null.
+        // for integers arrays the unassigned elements will be set to 0.
+
+        String[] newArr = {"hello", "hey", "hi", "hola"};
+
+        //better way of writing arrays because you don't have to separately add the elements.
+        // you can do all with in the braces {...} <--- elements must be of the same data type.
+
+        //simplify
+        int[] num; // (declaration) <--- declaring an array (num is getting defined in the stack)
+        num = new int[3]; // (initialization) <---- object is being created in the memory.
+
+               int[] needALongerName = new int[3];
+        //       |        |                 |
+        //       |        |                 |
+        //       v        v                 v
+        //(datatype) (ref variable) (creating obj in heap memory)
+        //
+        //  int[]  needALongerName  ---> occurs during compilation
+        //  new int[3]              ---> occurs during runtime - (Dynamic memory allocation)
+    }
+}
