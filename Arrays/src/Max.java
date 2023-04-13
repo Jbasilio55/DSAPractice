00dd6ae64eb8ce49e244134792d0ae9d64a2ccda
Jbasilio55 <j.l.basilio55@gmail.com>
2023-04-12 21:55:51 -0400

    completed Max and MaxRange w/ edge cases

--- Arrays/src/Max.java
+++ Arrays/src/Max.java
@@ -1,2 +1,33 @@
 public class Max {
+    public static void main(String[] args) {
+        int[] arr = {12, 34, 72, 66, 90, 32};
+        System.out.println(max(arr));
+        System.out.println(maxRange(arr, 0, 4));
+    }
+
+    private static int max(int[] arr) {
+        int max = arr[0];
+
+        for(int i = 1; i < arr.length; i++){
+            if(max < arr[i]){
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+
+    private static int maxRange(int[] arr, int start, int end) {
+        if(end > arr.length || end > start){
+            return -1;
+        }
+
+        int max = arr[start];
+
+        for(int i = 1; i < end; i++){
+            if(max < arr[i]){
+                max = arr[i];
+            }
+        }
+        return max;
+    }
 }
