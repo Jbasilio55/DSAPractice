ade7c8e0543eaedce05fabf61230fc04b12b8069
Jbasilio55 <j.l.basilio55@gmail.com>
2023-04-14 18:14:47 -0400

    Linear Search for 2D array (return boolean)

--- LinearSearch/src/SearchIn2DArray.java
+++ LinearSearch/src/SearchIn2DArray.java
@@ -9,6 +9,8 @@ public class SearchIn2DArray {
                 {13, 28}
         };
         System.out.println(Arrays.toString(search(arr, 55)));
+        System.out.println(search2(arr, 34));
+        System.out.println(search2(arr, 0));
     }
 
     public static int[] search(int[][] arr, int target){
@@ -27,5 +29,19 @@ public class SearchIn2DArray {
         }
         //return newArr
         return newArr;
+    }
+
+    public static boolean search2(int[][] arr, int target){
+        //Double for loop
+        for(int row = 0; row < arr.length; row++){
+            for(int col = 0; col < arr[row].length; col++){
+                //if target is found. return true
+                if(arr[row][col] == target){
+                    return true;
+                }
+            }
+        }
+        //return false / not found
+        return false;
     }
 }
