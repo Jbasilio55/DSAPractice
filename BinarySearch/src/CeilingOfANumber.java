f3fe5cf879804bf56c719f62fe64f986c0808db9
Jbasilio55 <j.l.basilio55@gmail.com>
2023-04-17 00:37:21 -0400

    change code, found bus and did not answer question

--- BinarySearch/src/CeilingOfANumber.java
+++ BinarySearch/src/CeilingOfANumber.java
@@ -4,11 +4,13 @@ public class CeilingOfANumber {
         // int[] numbs = {1, 2, 3, 4, 5, 6}, target = 4 | output 5
 
         int[] numbs = {1, 4, 7, 9, 13, 23, 27, 29, 33, 45};
-        System.out.println(ceiling(numbs, 13));
-        System.out.println(ceiling(numbs, 27));
+
+        System.out.println(ceiling2(numbs, 13));
+        System.out.println(ceiling2(numbs, 27));
+        System.out.println(ceiling2(numbs, 45));
     }
 
-    public static int ceiling(int[] arr, int target){
+    public static int ceiling2(int[] arr, int target){
         //make a start, middle, and end of the sorted array
         int start = 0;
         int end = arr.length - 1;
@@ -16,25 +18,21 @@ public class CeilingOfANumber {
 
         //Run while loop BS
         while(start <= end){
+            //Find middle element
             middle = start + (end - start) / 2;
-            //If target equals middle return it else return an element +1 greater
-            if(target == arr[middle]){
-                //If middle is at the end return it, instead of return arr.length + 1
-                if(middle == arr.length - 1){
-                    return arr[middle];
-                }
-                return arr[middle + 1];
-            }
-            //if target is greater than mid, then change start to be mid + 1
-            if(target > arr[middle]){
-                start = middle + 1;
-            }
-            //if target is less than mid, then change end to be mid + 1
+
+            //if target is greater than mid
             if(target < arr[middle]){
                 end = middle - 1;
+            } else if (target > arr[middle]) {
+                //if target is less than mid
+                start = middle + 1;
+            }else{
+                //else return mid
+                return arr[middle];
             }
         }
         //if element is not found return -1
-        return -1;
+        return arr[start];
     }
 }
---
+++ BinarySearch/src/FloorOfANumber.java
@@ -0,0 +1,5 @@
+public class FloorOfANumber {
+    public static void main(String[] args) {
+
+    }
+}
