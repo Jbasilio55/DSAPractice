a5c5a6e0f0530f90acb56bb6ccdf4aacab3d2be5
Jbasilio55 <j.l.basilio55@gmail.com>
2023-04-21 23:49:40 -0400

    updated Q. 33. code and added another method to run if there are duplicates in a rotated sorted array.

--- BinarySearch/src/RotatedSortedArray.java
+++ BinarySearch/src/RotatedSortedArray.java
@@ -15,7 +15,6 @@ public class RotatedSortedArray {
         //You must write an algorithm with O(log n) runtime complexity.
         //
         //
-        //
         //Example 1:
         //
         //Input: nums = [4,5,6,7,0,1,2], target = 0
@@ -86,6 +85,47 @@ public class RotatedSortedArray {
                 end = mid - 1;
             }else{
                 start = mid + 1;
+            }
+        }
+        return - 1;
+    }
+
+    public static int findPivotWithDuplicates(int[] arr){
+        int start = 0;
+        int end = arr.length - 1;
+        int mid;
+
+        while(start <= end){
+            mid = start + (end - start) / 2;
+
+            //4 cases
+            if(mid < end && arr[mid] > arr[mid + 1]){
+                return mid;
+            }
+            if(mid > start && arr[mid] < arr[mid - 1]){
+                return mid - 1;
+            }
+            // if element at the start, middle, end are all equals
+            if(arr[start] == arr[mid] && arr[mid] == arr[end]){
+                //skip the duplicates
+
+                //NOTE: What if one of the 3 elements (start, mid, end) were pivot
+                // check if start is pivot
+                if(arr[start]  > arr[start + 1]){
+                    return start;
+                }
+                start++;
+
+                if(arr[end]  < arr[end - 1]){
+                    return end - 1;
+                }
+                end--;
+            }
+            //left side is sorted, so pivot should be in the right
+            else if (arr[start] < arr[mid] || (arr[start] == arr[mid] && arr[mid] > arr[end])) {
+                start = mid + 1;
+            }else{
+                end = mid - 1;
             }
         }
         return - 1;
