9fd8729fdb5cc544c9c1ff6ba0c176bc3afc6eba
Jbasilio55 <j.l.basilio55@gmail.com>
2023-04-27 23:52:20 -0400

    Practice Cycle Sort

--- Sorting/src/CycleSort.java
+++ Sorting/src/CycleSort.java
@@ -1,2 +1,27 @@
+import java.util.Arrays;
+
 public class CycleSort {
+    public static void main(String[] args) {
+        int[] arr = {3, 5, 2, 1, 4};
+        cyclicSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void cyclicSort(int[] arr){
+        int i = 0;
+        int temp;
+        int correct;
+
+        while(i < arr.length){
+            correct = arr[i] - 1;
+
+            if(arr[i] != arr[correct]){
+                temp = arr[i];
+                arr[i] = arr[correct];
+                arr[correct] = temp;
+            }else{
+                i++;
+            }
+        }
+    }
 }
