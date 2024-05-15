0d642e9d87d5be1ae12381970605d1a1884e6041
Jbasilio55 <j.l.basilio55@gmail.com>
2024-05-15 13:09:03 -0400

    Currency conversion completed

--- Assignments/src/Day1/CurrencyConversion.java
+++ Assignments/src/Day1/CurrencyConversion.java
@@ -1,7 +1,32 @@
 package Day1;
 
+import java.util.Scanner;
+
 public class CurrencyConversion {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("---- Currency Conversion ----");
 
+        while(true){
+            // Ask if the current currency is USD or EUR
+            System.out.print("What kind of currency do you want to convert (USD) or (EUR): ");
+            String currency = scanner.nextLine();
+
+            if(currency.equals("USD") || currency.equals("EUR")) {
+                //ask for amount
+                System.out.print("Enter amount: ");
+                double amount = scanner.nextDouble();
+
+                if(currency.equals("USD")){
+                    double euro = amount * 0.919967;
+                    System.out.println("EUR: " + euro);
+                    break;
+                }else{
+                    double usd = amount * 1.08705;
+                    System.out.println("USD: " + usd);
+                    break;
+                }
+            }
+        }
     }
 }
