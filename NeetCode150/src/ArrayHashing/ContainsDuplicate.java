bf24c8c90eff6105f9747843f53ce69bdc3d8908
Jbasilio55 <j.l.basilio55@gmail.com>
2024-05-18 12:04:06 -0400

    leetcode 26 completed

--- NeetCode150/src/ArrayHashing/ContainsDuplicate.java
+++ NeetCode150/src/ArrayHashing/ContainsDuplicate.java
@@ -2,4 +2,7 @@
 package ArrayHashing;
 
 public class ContainsDuplicate {
+    public static void main(String[] args) {
+
+    }
 }
---
+++ NeetCode150/src/Practice/RemoveDuplicatesFromSA.java
@@ -0,0 +1,32 @@
+package Practice;
+
+// leetcode 26. Remove Duplicates from Sorted Array
+
+public class RemoveDuplicatesFromSA {
+    public static void main(String[] args) {
+        int test1[] = {1,1,2};
+        int test2[] = {0,0,1,1,1,2,2,3,3,4};
+
+        removeDuplicates(test1);
+        removeDuplicates(test2);
+
+        System.out.println(removeDuplicates(test1));
+        System.out.println(removeDuplicates(test2));
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        //start at index 1
+        int index = 1;
+
+        //iterate through array
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i] != nums[i - 1]){ // if element at 1 does not equal previous
+                nums[index] = nums[i]; // assign iterated element with element at "index"
+                index++; //increment index
+            }
+        }
+
+        //return num od unique values - k
+        return index;
+    }
+}
