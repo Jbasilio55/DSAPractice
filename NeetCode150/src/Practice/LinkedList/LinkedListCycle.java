047a357338b5f02e73623b0c58472ce429057b42
Jbasilio55 <j.l.basilio55@gmail.com>
2024-05-30 12:40:29 -0400

    141. Linked List Cycle - Easy (Completed)

---
+++ NeetCode150/src/Practice/LinkedList/LinkedListCycle.java
@@ -0,0 +1,28 @@
+package Practice.LinkedList;
+
+// 141. Linked List Cycle - Easy
+
+public class LinkedListCycle {
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+    public static boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow == fast){
+                return true;
+            }
+        }
+        return false;
+    }
+}
--- NeetCode150/src/Practice/LinkedList/MergeTwoSortedList.java
+++ NeetCode150/src/Practice/LinkedList/MergeTwoSortedList.java
@@ -1,5 +1,41 @@
 package Practice.LinkedList;
 
 public class MergeTwoSortedList {
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
 
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode node1 = list1;
+        ListNode node2 = list2;
+        ListNode completeList = new ListNode();
+
+        while(node1 != null && node2 != null){
+            if(node1.val < node2.val){
+                completeList.val = node1.val;
+                node1 = node1.next;
+            }else{
+                completeList.val = node2.val;
+                node2 = node2.next;
+            }
+            completeList = completeList.next;
+        }
+
+        while(node1 != null){
+            completeList.val = node1.val;
+            node1 = node1.next;
+            completeList = completeList.next;
+        }
+        while(node2 != null){
+            completeList.val = node2.val;
+            node2 = node2.next;
+            completeList = completeList.next;
+        }
+
+        return completeList;
+    }
 }
