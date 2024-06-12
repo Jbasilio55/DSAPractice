18305f207d04f6e1b41421c781cb3d671fce3da4
Jbasilio55 <j.l.basilio55@gmail.com>
2024-06-11 21:34:41 -0400

    Queues Practice

--- NeetCode150/src/Practice/Queues/PQueue.java
+++ NeetCode150/src/Practice/Queues/PQueue.java
@@ -1,5 +1,9 @@
 package Practice.Queues;
 
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 public class PQueue {
 
     public static void main(String[] args) {
@@ -7,5 +11,27 @@ public class PQueue {
         //                  with the highest priorities first
         //                  before elements with lower priority
 
+        Queue<Double> queue = new PriorityQueue<>(Collections.reverseOrder());
+        queue.offer(3.0);
+        queue.offer(2.5);
+        queue.offer(4.0);
+        queue.offer(1.5);
+        queue.offer(2.0);
+
+        while(!queue.isEmpty()){
+            System.out.println(queue.poll());
+        }
+
+        System.out.println();
+
+        Queue<String> queue2 = new PriorityQueue<>();
+        queue2.offer("Thomas");
+        queue2.offer("William");
+        queue2.offer("Adam");
+        queue2.offer("Beth");
+        queue2.offer("Sam");
+        queue2.offer("Evelyn");
+
+        System.out.println(queue2);
     }
 }
