4be07072afc6a340e4205fdd1c68657b6656d021
Jbasilio55 <j.l.basilio55@gmail.com>
2025-08-23 18:41:10 -0400

    Basic projects

---
+++ NeetCode150/src/Practice/Recursion/Main.java
@@ -0,0 +1,10 @@
+package Practice.Recursion;
+
+/**
+ * @author Jorge Basilio
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
--- NeetCode150/src/Practice/Stacks/LargeRectangleInHistogram.java
+++ NeetCode150/src/Practice/Stacks/LargeRectangleInHistogram.java
@@ -2,6 +2,6 @@ package Practice.Stacks;
 //84. Largest Rectangle in Histogram - Hard
 public class LargeRectangleInHistogram {
     public int largestRectangleArea(int[] heights) {
-
+        return -1;
     }
 }
